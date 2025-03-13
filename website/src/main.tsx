ad81907ff91b78164ee4344352a4ff01ae9a92f3
lucas espinosa <email@lucasespinosa.com>
2025-03-13 07:40:08 -0300

    build was fixed

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -5,7 +5,6 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 import './index.css'
-import App from './App.tsx'
 
 import '@rainbow-me/rainbowkit/styles.css';
 
